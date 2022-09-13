0e17d0ee514d88836f371c98ba5123a55c7b6675
Rémi Robichet <remi.robichet@edtake.com>
2022-09-13 17:25:27 +0200

    feat: add jwt builder

---
+++ functions/builder.js
@@ -0,0 +1,36 @@
+const jwt = require('jwt-simple');
+
+exports.handler = async event => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 404
+    }
+  }
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: `Missing body`
+    }
+  }
+  const body = JSON.parse(event.body)
+  if (!body.payload) {
+    return {
+      statusCode: 400,
+      body: `Missing body payload`
+    }
+  }
+  if (!body.signatureKey) {
+    return {
+      statusCode: 400,
+      body: `Missing body signature key`
+    }
+  }
+  const payload = body.payload
+  const signatureKey = body.signatureKey
+  const alg = event.body.algorithm || 'HS256'
+  const response = jwt.encode(payload, signatureKey, alg)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ jwt : response })
+  }
+}
